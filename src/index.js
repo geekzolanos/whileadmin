48c8af73bf378b90f048ad77c66a9434e78e0431
David Romero <geekzolanos@gmail.com>
2020-04-22 01:00:14 -0400

    Integracion de FIrebase

--- src/index.js
+++ src/index.js
@@ -1,17 +1,46 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+/*!
+
+=========================================================
+* Material Dashboard React - v1.8.0
+=========================================================
+
+* Product Page: https://www.creative-tim.com/product/material-dashboard-react
+* Copyright 2019 Creative Tim (https://www.creative-tim.com)
+* Licensed under MIT (https://github.com/creativetimofficial/material-dashboard-react/blob/master/LICENSE.md)
+
+* Coded by Creative Tim
+
+=========================================================
+
+* The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
+
+*/
+import React, { Suspense } from "react";
+import ReactDOM from "react-dom";
+import config from "config/firebase";
+import { createBrowserHistory } from "history";
+import { Router, Route, Switch, Redirect } from "react-router-dom";
+import { FirebaseAppProvider } from "reactfire";
+
+// core components
+import Admin from "layouts/Admin.js";
+
+import "assets/css/material-dashboard-react.css?v=1.8.0";
+import Login from "views/Login";
+
+const hist = createBrowserHistory();
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+  <FirebaseAppProvider firebaseConfig={config}>
+    <Suspense fallback={<h1>Please wait...</h1>}>
+      <Router history={hist}>
+        <Switch>
+          <Route path="/admin" component={Admin} />
+          <Route path="/login" component={Login} />
+          <Redirect from="/" to="/admin/dashboard" />
+        </Switch>
+      </Router>
+    </Suspense>
+  </FirebaseAppProvider>,
+  document.getElementById("root")
 );
-
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
