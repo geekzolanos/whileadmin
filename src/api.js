7b2e82b5309fa5e1893b9ed9ca5cf6d89dcf4173
David Romero <geekzolanos@gmail.com>
2020-05-03 03:22:58 -0400

    Actualizacion de metodos de api

--- src/api.js
+++ src/api.js
@@ -7,7 +7,7 @@ async function getCoursesByUser(firestore, user) {
     .get();
 
   const courses = snapshot.docs.map(doc =>
-    Object.assign({ ref: doc.ref }, doc.data())
+    Object.assign({ id: doc.id, ref: doc.ref }, doc.data())
   );
 
   return courses;
@@ -20,10 +20,9 @@ async function getTopicsByCourse(firestore, course) {
     .orderBy("createdAt", "desc")
     .get();
 
-  return snapshot.docs.map(doc => {
-    const data = Object.assign({ id: doc.id }, doc.data());
-    return data;
-  });
+  return snapshot.docs.map(doc =>
+    Object.assign({ id: doc.id, ref: doc.ref }, doc.data())
+  );
 }
 
 function getStudentsByCourse(course) {
@@ -35,4 +34,53 @@ function getStudentsByCourse(course) {
   );
 }
 
-export { getCoursesByUser, getTopicsByCourse, getStudentsByCourse };
+async function getProfileByStudents(functions, course) {
+  const res = await functions.httpsCallable("usersFromCourse")({
+    courseId: course.id
+  });
+
+  return res.data.result;
+}
+
+function getTopicContent(storage, course, topic) {
+  return storage
+    .ref(`courses/${course.id}/${topic.id}/content.md`)
+    .getDownloadURL()
+    .then(fetch)
+    .then(res => res.text());
+}
+
+function uploadTopicVideoThumb(storage, course, topic, blob) {
+  return storage
+    .ref(`thumbs/${course.id}/${topic.id}.jpg`)
+    .put(blob)
+    .then(ss => ss.ref.getDownloadURL());
+}
+
+function uploadTopicVideo(storage, course, topic, file) {
+  return storage.ref(`courses/${course.id}/${topic.id}/video.mp4`).put(file);
+}
+
+function uploadTopicContent(storage, course, topic, content) {
+  return storage
+    .ref(`courses/${course.id}/${topic.id}/content.md`)
+    .put(new Blob([content], { type: "text/markdown" }));
+}
+
+function getTopicVideo(storage, course, topic) {
+  return storage
+    .ref(`courses/${course.id}/${topic.id}/video.mp4`)
+    .getDownloadURL();
+}
+
+export {
+  getCoursesByUser,
+  getTopicsByCourse,
+  getStudentsByCourse,
+  getProfileByStudents,
+  getTopicContent,
+  uploadTopicVideoThumb,
+  uploadTopicVideo,
+  uploadTopicContent,
+  getTopicVideo
+};
