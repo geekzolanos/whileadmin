0a5da9509e44fc42236c93bba2225a07d0a93e9d
David Romero <geekzolanos@gmail.com>
2020-05-04 00:29:33 -0400

    [CourseForm] Implementacion de Formik

--- src/components/CourseDashboard/CourseForm.jsx
+++ src/components/CourseDashboard/CourseForm.jsx
@@ -1,94 +1,82 @@
-import React, { useState, useCallback, useEffect } from "react";
+import React from "react";
 import PropTypes from "prop-types";
 
 // @material-ui/core components
 import Grid from "@material-ui/core/Grid";
-import TextField from "@material-ui/core/TextField";
 import Button from "@material-ui/core/Button";
+import { Formik, Form, Field } from "formik";
+import { TextField } from "formik-material-ui";
 
 import Card from "../Card/Card.js";
 import CardHeader from "../Card/CardHeader.js";
 import CardBody from "../Card/CardBody.js";
-import CardFooter from "../Card/CardFooter.js";
+import Save from "@material-ui/icons/Save";
 
-const CourseForm = ({ course, ...props }) => {
-  const [state, setState] = useState({});
-
-  const handleSubmit = e => {
-    e.preventDefault();
-    props.onSubmit(state);
-  };
-
-  useEffect(() => {
-    setState(course);
-  }, [course]);
-
-  const handleChange = useCallback(
-    e => {
-      const target = e.target;
-      setState(p => {
-        const n = Object.assign({}, p);
-        n[target.name] = target.value;
-        return n;
-      });
-    },
-    [setState]
-  );
-
-  return (
-    <form onSubmit={handleSubmit}>
-      <Card>
-        <CardHeader>
-          <h4>Editar Curso</h4>
-          <p>Edite datos del curso</p>
-        </CardHeader>
-
-        <CardBody>
-          <Grid container spacing={4}>
-            <Grid item md={6}>
-              <TextField
-                name="name"
-                id="course__name"
-                label="Nombre"
-                value={state.name || ""}
-                onChange={handleChange}
-                fullWidth
-              />
+const CourseForm = ({ course, onSubmit }) => (
+  <Formik initialValues={course.data()} onSubmit={onSubmit}>
+    {({ isSubmitting }) => (
+      <Form>
+        <Card>
+          <CardHeader color="primary">
+            <Grid container alignItems="center">
+              <Grid item xs={12} md={true}>
+                <h4>Editar Curso</h4>
+                <p>Edite datos del curso</p>
+              </Grid>
+              <Grid item>
+                <Button
+                  type="submit"
+                  disabled={isSubmitting}
+                  startIcon={<Save />}
+                  style={{ color: "#fff" }}
+                >
+                  Guardar Cambios
+                </Button>
+              </Grid>
             </Grid>
-            <Grid item md={6}>
-              <TextField
-                name="duration"
-                id="course__duration"
-                label="Duracion"
-                value={state.duration || ""}
-                onChange={handleChange}
-                fullWidth
-              />
-            </Grid>
-            <Grid item md={12} style={{ marginTop: 8 }}>
-              <TextField
-                name="description"
-                id="course__description"
-                label="Descripcion"
-                fullWidth
-                multiline
-                rows={5}
-                value={state.description || ""}
-                onChange={handleChange}
-              />
-            </Grid>
-          </Grid>
-        </CardBody>
+          </CardHeader>
 
-        <CardFooter>
-          <Button color="primary" type="submit">
-            Guardar Cambios
-          </Button>
-        </CardFooter>
-      </Card>
-    </form>
-  );
-};
+          <CardBody>
+            <Grid container spacing={4}>
+              <Grid item md={6}>
+                <Field
+                  component={TextField}
+                  name="name"
+                  id="course__name"
+                  label="Nombre"
+                  variant="outlined"
+                  fullWidth
+                />
+              </Grid>
+              <Grid item md={6}>
+                <Field
+                  component={TextField}
+                  name="duration"
+                  id="course__duration"
+                  label="Duracion"
+                  variant="outlined"
+                  fullWidth
+                />
+              </Grid>
+              <Grid item md={12} style={{ marginTop: 8 }}>
+                <Field
+                  component={TextField}
+                  name="description"
+                  id="course__description"
+                  label="Descripcion"
+                  variant="outlined"
+                  fullWidth
+                  multiline
+                  rows={5}
+                />
+              </Grid>
+            </Grid>
+          </CardBody>
+        </Card>
+      </Form>
+    )}
+  </Formik>
+);
 
 CourseForm.propTypes = {
   course: PropTypes.any,
